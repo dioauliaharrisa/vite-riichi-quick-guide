59cb42f32d9e1c3a0d7a5f17554ccf84edce8c49
dioauliaharrisa <daharrisa@gmail.com>
2024-12-17 03:43:32 +0700

    'done'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 function App() {
-  return <>Hello</>;
+  return (
+    <MantineProvider>
+      <>Mantine</>
+    </MantineProvider>
+  );
 }
 
 export default App;
