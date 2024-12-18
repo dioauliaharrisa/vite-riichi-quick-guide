8fdf6728e7e6aca6688490e93df00b3b434b8231
dioauliaharrisa <daharrisa@gmail.com>
2024-12-18 16:08:06 +0700

    'done'

--- src/pages/kabe/page.tsx
+++ src/pages/kabe/page.tsx
@@ -13,10 +13,93 @@ export default function Page() {
           <div className={styles.discard_pond}>
             {createDiscardPond([
               [
+                "Z3",
+                "P2",
+                "Z4",
+                "P1",
+                "M2",
+                "Z5",
+                "M9",
                 "S2",
-                "S3",
+                "Z4",
+                "Z1",
+                "S9",
+                "S8",
+                "S7",
+                "S7",
+              ],
+              [
+                "Z4",
+                "M9",
+                "S8",
+                "Z4",
+                "Z6",
+                "P9",
+                "Z3",
                 "S4",
+                "S8",
+                "S6",
+                "P2",
+                "S7",
+                "S8",
+              ],
+              [
+                "Z3",
+                "S5",
+                "M9",
+                "Z5",
+                "P4",
+                "M1",
+                "M1",
+                "P9",
+                "P9",
+                "S9",
+                "Z2",
+                "S3",
                 "S2",
+                "P8",
+              ],
+              [
+                "Z3",
+                "Z5",
+                "S1",
+                "Z6",
+                "M1",
+                "S3",
+                "M2",
+                "Z1",
+                "S6",
+                "S6",
+                "S3",
+                "S1",
+                "S2",
+                "Z5",
+              ],
+            ])}
+          </div>
+          {createHand([
+            "M3",
+            "M3",
+            "M4",
+            "M5",
+            "M6",
+            "M6",
+            "M7",
+            "M8",
+            "Z7",
+            "Z7",
+            "M2",
+            "M3",
+            "M4",
+          ])}
+        </>
+      )}
+      {pageCounter === 1 && (
+        <>
+          <div className={styles.discard_pond}>
+            {createDiscardPond([
+              [
+                "S3",
                 "S3",
                 "S4",
                 "S2",
@@ -29,50 +112,27 @@ export default function Page() {
                 "S3",
                 "S4",
               ],
-              ["M2", "M3", "M4", "M7"],
+              ["M2", "M3", "M4"],
               ["P2", "P3", "P4"],
               ["Z2", "Z3", "Z4"],
             ])}
           </div>
           {createHand([
-            "P2",
-            "P4",
-            "S5",
-            "S0",
-            "S6",
+            "M3",
+            "M3",
+            "M4",
+            "M5",
+            "M6",
             "M6",
             "M7",
-            "Z2",
-            "Z4",
-            "M7",
-            "M7",
-            "M9",
-            "M9",
+            "M8",
+            "Z7",
+            "Z7",
+            "M2",
+            "M3",
+            "M4",
           ])}
         </>
-      )}
-      {pageCounter === 1 && (
-        <div className={styles.discard_pond}>
-          {createDiscardPond([
-            [
-              "S3",
-              "S3",
-              "S4",
-              "S2",
-              "S3",
-              "S4",
-              "S2",
-              "S3",
-              "S4",
-              "S2",
-              "S3",
-              "S4",
-            ],
-            ["M2", "M3", "M4"],
-            ["P2", "P3", "P4"],
-            ["Z2", "Z3", "Z4"],
-          ])}
-        </div>
       )}
     </div>
   );
