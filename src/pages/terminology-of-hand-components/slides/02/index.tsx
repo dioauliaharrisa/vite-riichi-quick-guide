165bca92d42f97939121167e09e91adce6dd2cc0
dioauliaharrisa <daharrisa@gmail.com>
2024-12-19 18:04:52 +0700

    'done'

--- src/pages/terminology-of-hand-components/slides/02/index.tsx
+++ src/pages/terminology-of-hand-components/slides/02/index.tsx
@@ -7,13 +7,13 @@ export const Slide02 = () => {
       <div className={styles.wrapper_lines}>
         {/* <div className={styles.line}> */}
         <div className={styles.line_1}>{createHand(["S2", "S3", "S4"])}</div>
-        {/* <div>Mentsu</div> */}
         {/* </div> */}
         <div className={styles.line_2}>{createHand(["M2", "M2", "M2"])}</div>
         <div className={styles.line_3}>{createHand(["P7", "P8", "P9"])}</div>
         <div className={styles.line_4}>{createHand(["Z3", "Z3"])}</div>
         <div className={styles.line_5}>{createHand(["P5", "Z4"])}</div>
       </div>
+      {/* <div className={styles.text}>Mentsu</div> */}
     </div>
   );
 };
