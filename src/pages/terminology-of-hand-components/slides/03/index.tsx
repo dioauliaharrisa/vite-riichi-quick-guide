b0b45a8d8fe572735a6404053815cabb81a5d421
dioauliaharrisa <daharrisa@gmail.com>
2024-12-22 15:58:27 +0700

    'done'

--- src/pages/terminology-of-hand-components/slides/03/index.tsx
+++ src/pages/terminology-of-hand-components/slides/03/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { createHand } from "../../../../helpers/createHands";
+import { Text } from "@mantine/core";
 
 export const Slide03 = () => {
   const lineRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const termRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [positions, setPositions] = useState<{ x: number; y: number }[]>([]);
 
   useEffect(() => {
@@ -47,6 +49,14 @@ export const Slide03 = () => {
     { className: "line_5", content: ["P5", "Z4"] },
   ];
 
+  const termsData = [
+    { name: "Floaters" },
+    { name: "Toitsu" },
+    { name: "Mentsu" },
+    { name: "Koutsu" },
+    { name: "Floaters" },
+  ];
+
   return (
     <div
       style={{
@@ -56,36 +66,69 @@ export const Slide03 = () => {
         height: "50vh",
         width: "100%",
         margin: "auto",
+        position: "relative",
         backgroundColor: "brown", // Testing background
       }}
     >
       <div
         style={{
           display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
           width: "100%",
         }}
       >
-        {linesData.map((line, index) => (
-          <div
-            key={index}
-            ref={(el) => (lineRefs.current[index] = el)}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "5rem",
-              height: "5rem",
-              backgroundColor: "blue", // Testing background
-              transform: positions[index]
-                ? `translate(${positions[index].x}vw, ${positions[index].y}vh)`
-                : "translate(0, 0)", // Apply final positions
-            }}
-          >
-            {createHand(line.content)}
-          </div>
-        ))}
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          {linesData.map((line, index) => (
+            <div
+              key={`hand-${index}`}
+              ref={(el) => (lineRefs.current[index] = el)}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "5rem",
+                height: "5rem",
+                // backgroundColor: "blue", // Testing background
+                transform: positions[index]
+                  ? `translate(${positions[index].x}vw, ${positions[index].y}vh)`
+                  : "translate(0, 0)", // Apply final positions
+              }}
+            >
+              {createHand(line.content)}
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            position: "absolute",
+          }}
+        >
+          {termsData.map((term, index) => (
+            <div
+              key={`terms-${index}`}
+              ref={(el) => (termRefs.current[index] = el)}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "5rem",
+                height: "5rem",
+                // backgroundColor: "yellow", // Testing background
+                transform: positions[index]
+                  ? `translate(${positions[index].x + 20}vw, ${
+                      positions[index].y
+                    }vh)`
+                  : "translate(0, 0)", // Apply final positions
+              }}
+            >
+              <Text>{term.name}</Text>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
