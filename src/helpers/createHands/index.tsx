07a3b825bca7d9e3e840484bf8b5fbef37fc436b
dioauliaharrisa <daharrisa@gmail.com>
2024-12-28 16:31:46 +0700

    'done'

--- src/helpers/createHands/index.tsx
+++ src/helpers/createHands/index.tsx
@@ -1,10 +1,31 @@
 import { TileImage } from "../../components/tile-image";
 import styles from "./index.module.css";
 
-export const createHand = ({ hand }: { hand: string[] }) => {
+export const createHand = ({
+  hand,
+  acceptance,
+}: {
+  hand: string[];
+  acceptance?: [];
+}) => {
   const mapped = hand.map((each, index) => {
-    const Component = <TileImage name={each} key={`wrapper ${each}-${index}`} />;
+    const Component = (
+      <TileImage name={each} key={`wrapper ${each}-${index}`} />
+    );
     return Component || null;
   });
-  return <div className={`wrapper_3d ${styles.wrapper}`}>{mapped}</div>;
+
+  // unused yet
+  const mappedAcceptance = acceptance?.map((each, index) => {
+    const Component = (
+      <TileImage name={each} key={`wrapper ${each}-${index}`} />
+    );
+    return Component || null;
+  });
+  return (
+    <div className={`wrapper_3d ${styles.wrapper}`}>
+      {mapped}
+      <div>{mappedAcceptance}</div>
+    </div>
+  );
 };
--- src/pages/headless-iishanten/page.tsx
+++ src/pages/headless-iishanten/page.tsx
@@ -4,6 +4,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import styles from "./index.module.css";
 import { createHand } from "../../helpers/createHands";
+import { Text } from "@mantine/core";
 
 export const HeadlessIishanten = () => {
   const pageCounter = usePageCounter((state) => state.pageCounter);
@@ -12,6 +13,7 @@ export const HeadlessIishanten = () => {
 
   const timelinePage0 = useRef(gsap.timeline({ paused: true }));
   const timelinePage1 = useRef(gsap.timeline({ paused: true }));
+  const timelinePage2 = useRef(gsap.timeline({ paused: true }));
 
   useGSAP(() => {
     // Timeline for page 0
@@ -20,26 +22,72 @@ export const HeadlessIishanten = () => {
       .set(".hand2", { display: "none" })
       .set(".hand3", { display: "none" })
       .fromTo(".discard1", { y: 0, opacity: 1 }, { y: -20, opacity: 0 })
-      .to(".right_part1", { x: -22 });
+      .to(".right_part1", { x: -25 });
 
     // Timeline for page 1
     timelinePage1.current
+      .set(".acceptance2", { display: "none" })
+      .set(".acceptance3", { display: "none" })
       .fromTo(".hand1", { display: "flex" }, { display: "none" })
       .fromTo(
         ".hand2",
         { display: "none", x: 0, y: 0 },
-        { display: "flex", x: 0, y: -45 }
+        { display: "flex", flexDirection: "column", x: 0, y: -65 }
       )
       .fromTo(
         ".hand3",
         { display: "none", x: 0, y: 0 },
-        { display: "flex", x: 0, y: 45 },
+        { display: "flex", flexDirection: "column", x: 0, y: 65 },
         "<"
       )
+      .to(".row", { display: "flex" }, "<")
       .to(".discard2", { y: -25, opacity: 0 })
       .to(".discard3", { y: -25, opacity: 0 }, "<")
-      .to(".right_part2", { x: -22 })
-      .to(".right_part3", { x: -22 }, "<");
+      .to(".right_part2", { x: -25 })
+      .to(".right_part3", { x: -25 }, "<")
+      .fromTo(
+        ".acceptance2",
+        { display: "none", opacity: 0, x: 0, y: 0 },
+        { display: "flex", opacity: 1, x: 0, y: 25 }
+      )
+      .fromTo(
+        ".acceptance3",
+        { display: "none", opacity: 0, x: 0, y: 0 },
+        { display: "flex", opacity: 1, x: 0, y: 25 },
+        "<"
+      );
+
+    timelinePage2.current
+      .set(".acceptance2", { display: "none" })
+      .set(".acceptance3", { display: "none" })
+      .fromTo(".hand1", { display: "flex" }, { display: "none" })
+      .fromTo(
+        ".hand2",
+        { display: "none", x: 0, y: 0 },
+        { display: "flex", flexDirection: "column", x: 0, y: -65 }
+      )
+      .fromTo(
+        ".hand3",
+        { display: "none", x: 0, y: 0 },
+        { display: "flex", flexDirection: "column", x: 0, y: 65 },
+        "<"
+      )
+      .to(".row", { display: "flex" }, "<")
+      .to(".discard2", { y: -25, opacity: 0 })
+      .to(".discard3", { y: -25, opacity: 0 }, "<")
+      .to(".right_part2", { x: -25 })
+      .to(".right_part3", { x: -25 }, "<")
+      .fromTo(
+        ".acceptance2",
+        { display: "none", opacity: 0, x: 0, y: 0 },
+        { display: "flex", opacity: 1, x: 0, y: 25 }
+      )
+      .fromTo(
+        ".acceptance3",
+        { display: "none", opacity: 0, x: 0, y: 0 },
+        { display: "flex", opacity: 1, x: 0, y: 25 },
+        "<"
+      );
   }, []);
 
   useEffect(() => {
@@ -49,8 +97,20 @@ export const HeadlessIishanten = () => {
     } else if (pageCounter === 1) {
       timelinePage0.current.pause(0).progress(0); // Reset page0 animation
       timelinePage1.current.restart(); // Restart page1 animation
+    } else if (pageCounter === 2) {
+      timelinePage0.current.pause(0).progress(0); // Reset page0 animation
+      timelinePage1.current.restart(); // Restart page1 animation
     }
   }, [pageCounter, timelinePage0, timelinePage1]);
+
+  const styleAcceptanceBox = {
+    // backgroundColor: "antiquewhite", // FOR TESTING
+    height: "5rem",
+    padding: "1rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+  };
 
   return (
     <div className={styles.page}>
@@ -73,50 +133,80 @@ export const HeadlessIishanten = () => {
           </div>
         </div>
         <div className="hand2">
-          <div className="left_part2">
-            {createHand({
-              hand: [
-                "S2",
-                "S3",
-                "S4",
-                "M7",
-                "M8",
-                "M9",
-                "M9",
-                "P7",
-                "P9",
-                "S7",
-                "S8",
-                "S9",
-              ],
-            })}
+          <div className="row">
+            <div className="left_part2">
+              {createHand({
+                hand: [
+                  "S2",
+                  "S3",
+                  "S4",
+                  "M7",
+                  "M8",
+                  "M9",
+                  "M9",
+                  "P7",
+                  "P9",
+                  "S7",
+                  "S8",
+                  "S9",
+                ],
+              })}
+            </div>
+            <div className="discard2">
+              {createHand({
+                hand: ["M1"],
+              })}
+            </div>
+            <div className="right_part2">
+              {createHand({
+                hand: ["M2"],
+              })}
+            </div>
           </div>
-          <div className="discard2">
+          <div className="acceptance2" style={styleAcceptanceBox}>
             {createHand({
-              hand: ["M1"],
+              hand: ["P8", "M2", "M6", "M9"],
             })}
-          </div>
-          <div className="right_part2">
-            {createHand({
-              hand: ["M2"],
-            })}
+            <Text
+              size="xl"
+              fw={900}
+              variant="gradient"
+              gradient={{ from: "blue", to: "cyan", deg: 90 }}
+            >
+              13 Ukeire
+            </Text>
           </div>
         </div>
         <div className="hand3">
-          <div className="left_part3">
-            {createHand({
-              hand: ["S2", "S3", "S4", "M7", "M8", "M9"],
-            })}
+          <div className="row">
+            <div className="left_part3">
+              {createHand({
+                hand: ["S2", "S3", "S4", "M7", "M8", "M9"],
+              })}
+            </div>
+            <div className="discard3">
+              {createHand({
+                hand: ["M9"],
+              })}
+            </div>
+            <div className="right_part3">
+              {createHand({
+                hand: ["P7", "P9", "S7", "S8", "S9", "M1", "M2"],
+              })}
+            </div>
           </div>
-          <div className="discard3">
+          <div className="acceptance3" style={styleAcceptanceBox}>
             {createHand({
-              hand: ["M9"],
+              hand: ["M1", "M2", "M3", "P7", "P8", "P9"],
             })}
-          </div>
-          <div className="right_part3">
-            {createHand({
-              hand: ["P7", "P9", "S7", "S8", "S9", "M1", "M2"],
-            })}
+            <Text
+              size="xl"
+              fw={900}
+              variant="gradient"
+              gradient={{ from: "blue", to: "cyan", deg: 90 }}
+            >
+              20 Ukeire
+            </Text>
           </div>
         </div>
       </div>
